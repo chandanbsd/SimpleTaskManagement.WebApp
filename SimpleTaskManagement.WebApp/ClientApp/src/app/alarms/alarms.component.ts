eb09356c3d4fd898aa75bfe13ebd669ffb58ec40
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2024-06-30 09:24:27 -0400

    Added title bar and components for task management

---
+++ SimpleTaskManagement.WebApp/ClientApp/src/app/alarms/alarms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alarms',
+  standalone: true,
+  imports: [],
+  templateUrl: './alarms.component.html',
+  styleUrl: './alarms.component.css'
+})
+export class AlarmsComponent {
+
+}
--- SimpleTaskManagement.WebApp/ClientApp/src/app/app-router.module.ts
+++ SimpleTaskManagement.WebApp/ClientApp/src/app/app-router.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {ToDoListComponent} from "./to-do-list/to-do-list.component";
+import {RemindersComponent} from "./reminders/reminders.component";
+import {AlarmsComponent} from "./alarms/alarms.component";
+import {NotesComponent} from "./notes/notes.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'ToDoList', component: ToDoListComponent, pathMatch: 'full' },
+  { path: 'Reminders', component: RemindersComponent, pathMatch: 'full' },
+  { path: 'Alarms', component: AlarmsComponent, pathMatch: 'full' },
+  { path: 'Notes', component: NotesComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
